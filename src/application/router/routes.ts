5f578e798b7e61c2dcfe10c3bea89c2630d7d181
kcygan <kacper.cygan@netguru.com>
2021-09-06 18:30:13 +0200

    fix: Changed create route to object

--- src/application/router/routes.ts
+++ src/application/router/routes.ts
@@ -5,6 +5,10 @@ export const CONTRACTS_MAIN_ROUTE = {
 
 export const CONTRACTS_ROUTES = {
   contracts: CONTRACTS_MAIN_ROUTE,
+  create: {
+    path: `${CONTRACTS_MAIN_ROUTE.path}/create`,
+    name: 'Create contract',
+  },
 };
 
 export const navRoutes = [CONTRACTS_MAIN_ROUTE];
