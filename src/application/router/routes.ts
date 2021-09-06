8ad732c2de0c5932e07ff9b38acb01472f56beca
kcygan <kacper.cygan@netguru.com>
2021-09-06 17:09:20 +0200

    feat: Added routes for the contracts domain

---
+++ src/application/router/routes.ts
@@ -0,0 +1,10 @@
+export const CONTRACTS_MAIN_ROUTE = {
+  path: '/contracts',
+  name: 'Contracts manager',
+};
+
+export const CONTRACTS_ROUTES = {
+  contracts: CONTRACTS_MAIN_ROUTE,
+};
+
+export const navRoutes = [CONTRACTS_MAIN_ROUTE];
